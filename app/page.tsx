ec85ba8666b0a76ace33fc1e28852d438e40f9da
VeronicaDC87 <152705316+VeronicaDC87@users.noreply.github.com>
2024-02-16 14:51:03 +0000

    tempupdate

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,8 @@ import { useQuery } from "react-query";
 import axios from "axios";
 import { format, formatDistanceStrict, formatDistanceToNow, formatDistanceToNowStrict, formatISO, parseISO } from "date-fns";
 import { formatDistance } from "date-fns/fp";
+import Container from "./Container";
+import { convertoKelvintoCelcius } from "./component/Utils/convertKelvintoCelcius";
 
 type WeatherData = {
   cod: string;
@@ -105,12 +107,40 @@ const FirstData = data?.list[0]
   <div className="flex flex-col gap-4 bg-gray-100 min-h-screen">
     <Navbar />
     <main className="px-3 max-w-7xl mx-auto flax flex-col gap-9 w-full pb-10 pt-4">
-      <section>
-        <div>
+      <section className="space-y-4">
+        <div className="space-y-2">
           <h2 className="flex gap-1 text-2xl items-end">
-            <p> {format(parseISO(FirstData?.dt_txt ?? ""), "EEEE")} </p> 
+            <p> {format(parseISO(FirstData?.dt_txt ?? ""), "EEEE")} </p>
+            <p className="text-lg"> ({format(parseISO(FirstData?.dt_txt ?? ""), "dd.MM.yyyy")}) </p> 
 
           </h2>
+          <Container className="gap-10 px-6 items-centered">
+            <div className="flex flex-col px-4 ">
+            <div className="flex gap-10 sm:gap-16 overflow-x-auto w-full justify-between pr-3 "></div>
+              {data?.list.map((d,i) => (
+                <div key={i}
+                className="flex flex-col justify-between gap-2 items-center text-xs font-semibold"></div>
+              ))}  
+            <span className="text-5-xl">
+              {convertoKelvintoCelcius(FirstData?.main.temp ?? 296.37)}°
+              </span>
+              <p className="text-xs space-x-1 whitespace-nowrap">
+              <span> Feels like </span>
+              <span>
+              {convertoKelvintoCelcius(FirstData?.main.feels_like ?? 0)}° 
+              </span>
+              </p>
+              <p className="text-xs space-x-2">
+              <span>
+              {convertoKelvintoCelcius(FirstData?.main.temp_min ?? 0)}°↓ {" "} 
+              </span>
+              <span>
+              {" "}
+              {convertoKelvintoCelcius(FirstData?.main.temp_max ?? 0)}°↑  
+              </span>
+              </p>
+            </div>
+          </Container>
         </div>
       </section>
       <section></section>
